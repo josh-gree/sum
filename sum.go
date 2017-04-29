f23169b43d6e5f9325fbb7cba9173731bd266ba5
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-04-29 23:10:38 +0100

    sum service

---
+++ sum.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/josh-gree/comm"
+	"math/rand"
+	"time"
+)
+
+var j = comm.JobMessage{}
+var r = comm.ResMessage{}
+var public = false // read from cml
+
+func Sum(data []float64,id int){
+	sum := 0.0
+	for _,d := range data{
+		sum += d
+	}
+	time.Sleep(time.Duration(rand.Int31n(10000)) * time.Millisecond)
+
+	resmsg := comm.ResMessage{Id:id,Result:sum}
+
+	resmsg.Send("localhost:8000/res")
+}
+
+func main(){
+	e := echo.New()
+
+	e.POST("/job", j.Recieve(public,Sum))
+
+	e.Start(":8001")
+}
