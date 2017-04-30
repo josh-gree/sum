dad61bd86aa8aedbd00f96fa145538dbadaf9ece
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-04-30 18:54:47 +0100

    gofmt

--- sum.go
+++ sum.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-	"github.com/labstack/echo"
-	"github.com/josh-gree/comm"
+	"fmt"
 	"math/rand"
 	"time"
-	"fmt"
+
+	"github.com/josh-gree/comm"
+	"github.com/labstack/echo"
 )
 
 var j = comm.JobMessage{}
 var r = comm.ResMessage{}
 var public = false // read from cml
 
-func Sum(data []float64,id int){
+func Sum(data []float64, id int) {
 	sum := 0.0
-	for _,d := range data{
+	for _, d := range data {
 		sum += d
 	}
 	time.Sleep(time.Duration(rand.Int31n(10000)) * time.Millisecond)
 
-	resmsg := comm.ResMessage{Id:id,Result:sum}
+	resmsg := comm.ResMessage{Id: id, Result: sum}
 
 	resmsg.Send("public:8000/res")
 }
 
-func main(){
+func main() {
 	fmt.Println("Hello!")
 	e := echo.New()
 
-	e.POST("/job", j.Recieve(public,Sum))
+	e.POST("/job", j.Recieve(public, Sum))
 
 	e.Start(":8000")
 }
